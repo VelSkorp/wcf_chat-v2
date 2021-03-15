a0db24f5b5de4ac06dbae700b57afca4d3c7adf0
Влад Концевич <kontsevichv@mail.ru>
2021-03-15 14:57:26 +0300

    Implemented ServerPage UI

--- ChatHost.Core/ViewModel/Application/ServerViewModel.cs
+++ ChatHost.Core/ViewModel/Application/ServerViewModel.cs
@@ -1,4 +1,7 @@
-﻿namespace ChatHost.Core
+﻿using System.Collections.Generic;
+using System.Windows.Input;
+
+namespace ChatHost.Core
 {
 	/// <summary>
 	/// The View Model for a login screen
@@ -7,11 +10,39 @@
 	{
 		#region Public Properties
 
+		/// <summary>
+		/// A list of users
+		/// </summary>
+		public List<string> UsersList { get; set; }
+
+		/// <summary>
+		/// The user selected in the ListBox
+		/// </summary>
+		public string SelectedUser { get; set; }
+
+		/// <summary>
+		/// The server message log
+		/// </summary>
+		public List<string> Log { get; set; }
 
 		#endregion
 
 		#region Commands
 
+		/// <summary>
+		/// The command to disable user
+		/// </summary>
+		public ICommand DisableUserCommand { get; set; }
+
+		/// <summary>
+		/// The command to start the server
+		/// </summary>
+		public ICommand StartTheServerCommand { get; set; }
+
+		/// <summary>
+		/// The command to refresh user list
+		/// </summary>
+		public ICommand RefreshUserListCommand { get; set; }
 
 		#endregion
 
@@ -23,8 +54,32 @@
 		public ServerViewModel()
 		{
 			// Create commands
+			DisableUserCommand = new RelayCommand(DisableUser);
+			StartTheServerCommand = new RelayCommand(StartTheServer);
+			RefreshUserListCommand = new RelayCommand(RefreshUserList);
 		}
 
 		#endregion
+
+		/// <summary>
+		/// Disable user
+		/// </summary>
+		public void DisableUser()
+		{
+		}
+
+		/// <summary>
+		/// Start the server
+		/// </summary>
+		public void StartTheServer()
+		{
+		}
+
+		/// <summary>
+		/// Refresh user list
+		/// </summary>
+		public void RefreshUserList()
+		{
+		}
 	}
 }
