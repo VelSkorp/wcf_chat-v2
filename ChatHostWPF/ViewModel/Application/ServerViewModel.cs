4e8331245793e15cd6d489218be820c9e323742b
Влад Концевич <kontsevichv@mail.ru>
2021-05-26 15:17:26 +0300

    Implemented self-hosting of WCF service ServiceChat

--- ChatHostWPF/ViewModel/Application/ServerViewModel.cs
+++ ChatHostWPF/ViewModel/Application/ServerViewModel.cs
@@ -1,4 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using Chat.Core;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.ServiceModel;
+using System.ServiceModel.Discovery;
 using System.Windows.Input;
 
 namespace ChatHostWPF
@@ -13,12 +18,21 @@ namespace ChatHostWPF
 		/// <summary>
 		/// The flag that indicates server is started or not
 		/// </summary>
-		public bool IsStarted { get; set; } = false;
+		public bool IsStarted { get; private set; } = false;
 
 		/// <summary>
 		/// The server message log
 		/// </summary>
-		public List<string> Log { get; set; }
+		public List<string> Log { get; private set; } = new List<string>();
+
+		#endregion
+
+		#region Private Members
+
+		/// <summary>
+		/// ServiceHost for hosting ServiceChat service
+		/// </summary>
+		private ServiceHost mServiceHost;
 
 		#endregion
 
@@ -57,7 +71,36 @@ namespace ChatHostWPF
 		/// </summary>
 		public void StartTheServer()
 		{
-			IsStarted = true;
+			try
+			{
+				// Create base adress for ServiceHost using DNS hostname of the local computer
+				var baseAdress = new Uri($"net.tcp://{Dns.GetHostName()}");
+
+				// Create a ServiceHost for the ServiceChat type.  
+				mServiceHost = new ServiceHost(ServiceChat.Instance, baseAdress);
+
+				// Add ServiceDiscoveryBehavior  
+				mServiceHost.Description.Behaviors.Add(new ServiceDiscoveryBehavior());
+
+				// Add a UdpDiscoveryEndpoint  
+				mServiceHost.AddServiceEndpoint(new UdpDiscoveryEndpoint());
+
+				// Open the ServiceHost to create listeners
+				// and start listening for messages.  
+				mServiceHost.Open();
+
+				IsStarted = true;
+
+				Log.Add($"[Data] Host stated\n");
+			}
+			catch (Exception ex)
+			{
+				Log.Add($"[Error] {ex.Message}\n");
+			}
+			finally
+			{
+				Log = new List<string>(Log);
+			}
 		}
 
 		/// <summary>
@@ -65,8 +108,24 @@ namespace ChatHostWPF
 		/// </summary>
 		public void DisableTheServer()
 		{
-			IsStarted = false;
-		} 
+			try
+			{
+				// Close the ServiceHost
+				mServiceHost.Close();
+
+				IsStarted = false;
+
+				Log.Add($"[Data] Host stoped\n");
+			}
+			catch (Exception ex)
+			{
+				Log.Add($"[Error] {ex.Message}\n");
+			}
+			finally
+			{
+				Log = new List<string>(Log);
+			}
+		}
 
 		#endregion
 	}
